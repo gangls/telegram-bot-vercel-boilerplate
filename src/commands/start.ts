ee8e47f2e188e17311d8bd9585e523ae63c8554e
shgli <shgli@sending.tech>
2025-03-14 15:22:30 +0800

    fix config

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -5,16 +5,28 @@ import { author, name, version } from '../../package.json';
 
 const debug = createDebug('bot:about_command');
 
-const about = () => async (ctx: Context) => {
+const start = () => async (ctx: Context) => {
   const keyboard = {
     inline_keyboard: [
       [{ text: '订阅', callback_data: 'button1' },{ text: '取消订阅', callback_data: 'button1' }],
     ]
   };
 
+  const welcomeText = `
+🌟 *欢迎来到超级机器人！* 🌟
+
+我是你的智能助手，可以帮你完成以下任务：
+- 📅 管理日程
+- 🌐 搜索信息
+- 🎮 玩游戏
+- 🛠️ 提供实用工具
+
+点击下面的按钮开始吧！
+`;
+
   const message = `*${name} ${version}*\n${author}`;
   debug(`Triggered "about" command with message \n${message}`);
-  await ctx.replyWithMarkdownV2(message, { parse_mode: 'Markdown', reply_markup: keyboard });
+  await ctx.replyWithMarkdownV2(welcomeText, { parse_mode: 'Markdown', reply_markup: keyboard });
 };
 
-export { about };
+export { start };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Telegraf } from 'telegraf';
 import {HttpsProxyAgent} from 'https-proxy-agent'
 
-import { about } from './commands';
+import { about, start } from './commands';
 import { greeting } from './text';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { development, production } from './core';
@@ -9,15 +9,24 @@ import { development, production } from './core';
 const BOT_TOKEN = process.env.BOT_TOKEN || '';
 const ENVIRONMENT = process.env.NODE_ENV || '';
 
-const bot = new Telegraf(BOT_TOKEN);
+const bot = new Telegraf(BOT_TOKEN, {
+    telegram: {
+        agent: new HttpsProxyAgent(process.env.PROXY_URL||"http://127.0.0.1:54706")
+    }
+});
 
 bot.telegram.setMyCommands([
     {
       command: 'about',
       description: 'Test command',
     },
+    {
+      command: 'start',
+      description: 'Test command',
+    },
   ]);
 
+bot.command('start', start());
 bot.command('about', about());
 bot.on('message', greeting());
 
